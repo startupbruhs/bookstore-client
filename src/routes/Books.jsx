11378278c364f1fc8cad6dae6a92d171e58bc068
Eraldo Forgoli <eraldoforgoli@gmail.com>
2019-12-01 15:02:05 +0100

    add key to books

--- src/routes/Books.jsx
+++ src/routes/Books.jsx
@@ -5,18 +5,18 @@ import { Row } from "antd";
 
 const Books = () => {
   const fakeBooks = [
-    { name: "bookName", author: "eraldo", imagePath: goat },
-    { name: "bookName", author: "eraldo", imagePath: goat },
-    { name: "bookName", author: "eraldo", imagePath: goat },
-    { name: "bookName", author: "eraldo", imagePath: goat },
-    { name: "bookName", author: "eraldo", imagePath: goat },
-    { name: "bookName", author: "eraldo", imagePath: goat },
-    { name: "bookName", author: "eraldo", imagePath: goat },
-    { name: "bookName", author: "eraldo", imagePath: goat },
-    { name: "bookName", author: "eraldo", imagePath: goat }
+    { name: "bookName", author: "eraldo", imagePath: goat, id: "afsdfds" },
+    { name: "bookName", author: "eraldo", imagePath: goat, id: "afsasdfds" },
+    { name: "bookName", author: "eraldo", imagePath: goat, id: "asfdsa" },
+    { name: "bookName", author: "eraldo", imagePath: goat, id: "afsaasdfds" },
+    { name: "bookName", author: "eraldo", imagePath: goat, id: "afs33dfds" },
+    { name: "bookName", author: "eraldo", imagePath: goat, id: "afs22dfds" },
+    { name: "bookName", author: "eraldo", imagePath: goat, id: "afsd11fds" },
+    { name: "bookName", author: "eraldo", imagePath: goat, id: "afsdf543ds" },
+    { name: "bookName", author: "eraldo", imagePath: goat, id: "afs1133dfds" }
   ];
 
-  const books = fakeBooks.map(book => <Book book={book} />);
+  const books = fakeBooks.map(book => <Book key={book.id} book={book} />);
 
   return <Row gutter={24}>{books}</Row>;
 };
