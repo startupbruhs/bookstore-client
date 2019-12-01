cfb67463ac6e90fc2d29545db16da12a573eca86
Bernard <bernardkllogjri@gmail.com>
2019-12-01 13:03:48 +0100

    initial commit

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,44 +1,45 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import loadable from "@loadable/component";
-import Loading from "./Loading.js";
+import Loading from "./Loading";
 
-const Home = loadable(() => import("./Home.js"), {
-  fallback: <Loading />
-});
+const Home = () =>
+  loadable(() => import("./Home"), {
+    fallback: <Loading />
+  });
 
-const About = loadable(() => import("./About.js"), {
-  fallback: <Loading />
-});
+const About = () =>
+  loadable(() => import("./About"), {
+    fallback: <Loading />
+  });
 
-const Contact = loadable(() => import("./Contact.js"), {
-  fallback: <Loading />
-});
+const Contact = () =>
+  loadable(() => import("./Contact"), {
+    fallback: <Loading />
+  });
 
-export default function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
+export default () => (
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
 
-        <Switch>
-          <Route path="/home" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contactn" component={Contact} />
-        </Switch>
-      </div>
-    </Router>
-  );
-}
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
+    </div>
+  </Router>
+);
