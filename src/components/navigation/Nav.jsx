33b2d3d124f6f4a7f35ae7dc0471478ed3a083bf
Eraldo Forgoli <eraldoforgoli@gmail.com>
2019-12-01 16:47:13 +0100

    select navvar routed on page load"

--- src/components/navigation/Nav.jsx
+++ src/components/navigation/Nav.jsx
@@ -1,24 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Menu, Layout } from "antd";
 import { menu } from "./utils";
+import { Switch, useLocation } from "react-router-dom";
 
 const { Sider } = Layout;
 
 export const Nav = ({ theme, links }) => {
-  const [current, setCurrent] = useState("1");
+  const [current, setCurrent] = useState("0");
 
   const handleClick = e => {
     setCurrent(e.key);
   };
 
+  let location = useLocation();
+
+  useEffect(() => {
+    setCurrent(links.findIndex(i => location.pathname === i.path).toString());
+  }, []);
+
   return (
     <Sider width={200} style={{ background: "#fff" }}>
       <Menu
         theme={theme}
-        defaultSelectedKeys={["1"]}
+        defaultSelectedKeys={[current]}
         onClick={handleClick}
         style={{ height: "100%", borderRight: 0 }}
-        defaultOpenKeys={["sub1"]}
+        defaultOpenKeys={[current]}
         selectedKeys={[current]}
         mode="inline"
       >
