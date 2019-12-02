3623b1700a7cd3040e58c766f23edfef9735e04f
Eraldo Forgoli <eraldoforgoli@gmail.com>
2019-12-02 14:15:58 +0100

    disable booked dates on calendar

--- src/components/RangePicker.jsx
+++ src/components/RangePicker.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { DatePicker } from "antd";
+import moment from "moment";
 const { RangePicker: Picker } = DatePicker;
 
-const RangePicker = {disabledDates} => {
+const RangePicker = ({ disabledDates }) => {
   const onChange = (date, dateString) => {
     console.log(date);
     console.log(dateString);
   };
 
-  return <Picker onChange={onChange} disabledDate={disabledDates} />;
+  const disableDates = current => {
+    let index = disabledDates.findIndex(
+      date => date === moment(current).format("YYYY-MM-DD")
+    );
+    return index === 1;
+  };
+
+  return <Picker onChange={onChange} disabledDate={disableDates} />;
 };
 
 export default RangePicker;
