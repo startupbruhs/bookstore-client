19334cfd1b56475117e959dfb338a6668b867945
Eraldo Forgoli <eraldoforgoli@gmail.com>
2019-12-02 21:55:13 +0100

    disable book button if dates intercept

--- src/components/RangePicker.jsx
+++ src/components/RangePicker.jsx
@@ -5,10 +5,27 @@ const { RangePicker: Picker } = DatePicker;
 
 const RangePicker = ({ disabledDates, setIsDateSelected }) => {
   const onChange = (date, dateString) => {
-    setIsDateSelected(true);
-    console.log(date);
     console.log(dateString);
+
+    if (datesIntercept(disabledDates, dateString)) {
+      alert("dates intercept, you can't book");
+    } else setIsDateSelected(true);
   };
+
+  function datesIntercept(disabledDates, selectedRange) {
+    const firstDate = moment(selectedRange[0]).format("YYYY-MM-DD");
+    const lastDate = moment(selectedRange[1]).format("YYYY-MM-DD");
+    for (let i = 0; i < disabledDates.length; i++) {
+      let date = moment(disabledDates[i]).format("YYYY-MM-DD");
+
+      if (
+        moment(date).isAfter(firstDate, "day") &&
+        moment(date).isBefore(lastDate, "day")
+      )
+        return true;
+    }
+    return false;
+  }
 
   const disableDates = current => {
     return disabledDates.find(
